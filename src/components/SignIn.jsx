da6d4126a01bc6a279bcd9f5cb488e41463e6536
David Puerta Martos <dpuerta@usal.es>
2022-12-02 20:11:34 +0100

    Exercise 10.8

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,7 +1,65 @@
-import Text from './Text';
+import React from 'react';
+import { Formik } from 'formik';
+import { View, TouchableWithoutFeedback, Text, StyleSheet } from 'react-native';
+
+import FormikTextInput from './FormikTextInput';
+import theme from '../theme';
+
+const styles = StyleSheet.create({
+  container: { backgroundColor: theme.colors.white },
+  form: {
+    padding: 20
+  },
+  submit: {
+    backgroundColor: theme.colors.primary,
+    padding: 10,
+    borderRadius: 5,
+    color: "white",
+    fontWeight: theme.fontWeights.bold,
+    textAlign: "center",
+    marginVertical: 10
+  }
+});
+const initialValues = {
+  username: "",
+  password: ""
+};
+
+const SignInForm = ({ onSubmit }) => {
+  return (
+    <View style={styles.form}>
+      <FormikTextInput name="username" placeholder="Username" />
+      <FormikTextInput name="password" placeholder="Password" secureTextEntry />
+      <TouchableWithoutFeedback onPress={onSubmit}>
+        <View>
+          <Text style={styles.submit}>Sign in</Text>
+        </View>
+      </TouchableWithoutFeedback>
+    </View>
+  );
+};
+
+
 
 const SignIn = () => {
-  return <Text>The sign in view</Text>;
+  const onSubmit = values => {
+    console.log(values);
+    // const mass = parseFloat(values.mass);
+    // const height = parseFloat(values.height);
+
+    // if (!isNaN(mass) && !isNaN(height) && height !== 0) {
+    //   console.log(`Your body mass index is: ${getBodyMassIndex(mass, height)}`);
+    // }
+
+  };
+
+  return (
+    <View style={styles.container}>
+      <Formik initialValues={initialValues} onSubmit={onSubmit}  >
+        {({ handleSubmit }) => <SignInForm onSubmit={handleSubmit} />}
+      </Formik>
+    </View>
+  );
 };
 
 export default SignIn;
